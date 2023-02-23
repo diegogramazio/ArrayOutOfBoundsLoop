be9c3c67f827a41007e7c760eb2ed4bea6d1bb77
diegogramazio <116880984+diegogramazio@users.noreply.github.com>
2023-02-23 16:50:40 +0100

    Create ArrayOutOfBoundsLoop.c

---
+++ ArrayOutOfBoundsLoop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+      int i;
+      int interi[3];
+      for(i=0; i<=3; i++) {
+            printf("inizializzo elemento %d\n", i);
+            interi[i] = 0;
+            system("PAUSE");
+      }
+}
